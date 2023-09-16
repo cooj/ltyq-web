7b44735fc0a903dc13f52e42fc0fb0a4d6561ea8
cooj <z8888788@163.com>
2023-09-16 17:07:20 +0800

    build(config): 文件保存环境变量配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,10 +9,6 @@ module.exports = {
             env: {
                 // 数据库
                 DATABASE_URL: 'mysql://root:root@localhost:3306/nuxt-prisma',
-                // # 文件上传主机地址
-                NUXT_UPLOAD_HOST: 'http://localhost:9008',
-                // # 文件上传保存路径
-                NUXT_UPLOAD_DIR: '/public/upload',
             },
         },
     ],
