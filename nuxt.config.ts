4f55d89f5572b2d828d5aa1a957911e19af96ef5
cooj <z8888788@163.com>
2023-09-18 09:20:55 +0800

    fix(config): api接口跨域配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,7 +38,7 @@ export default defineNuxtConfig({
             // ignore: ['/hi'],
         },
         routeRules: {
-            '/api/**': { cors: true },
+            '/api/**': { cors: true, headers: { 'access-control-allow-origin': '*' } },
         },
 
         // experimental: {
